2628820cc20c6ada1b61d4fa8aed835106d560bc
YevaUsatova <yeva.usatova@gmail.com>
2021-05-19 00:48:52 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    const spy = receivesAFunction(callback);
+    return spy;
+    //returnsANamedFunction(callback);
+    //returnsAnAnonymousFunction(callback);
+}
+function returnsANamedFunction() {
+     let name = " fun";
+     return name;
+}
+function returnsAnAnonymousFunction(){
+     let name = " ''";
+     return name;
+}
